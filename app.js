c57fde9856e9ef0da9c6e9640e80c707df5e1ad4
Luca Nealon <lucan@tamu.edu>
2025-12-18 18:20:56 +0100

    connected to firebase

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // ----------- Firebase Setup -------------
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyC-fWhhmVQ8ycZ7-JKe3JMkEaDpaHOohXY",
+  authDomain: "pwa-site-6a264.firebaseapp.com",
+  projectId: "pwa-site-6a264",
+  storageBucket: "pwa-site-6a264.firebasestorage.app",
+  messagingSenderId: "866725666680",
+  appId: "1:866725666680:web:f359bb94f75903eb6df28d",
+  measurementId: "G-EVWCLQM7L8"
 };
 
 firebase.initializeApp(firebaseConfig);
